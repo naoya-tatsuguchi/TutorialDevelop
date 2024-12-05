efe800eb4cd2713bcb874f2bac12ecc86e307a1e
naoya.tatsuguchi <tkn-rk.atg115@docomo.ne.jp>
2024-12-05 12:33:20 +0900

    Lesson18

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,6 +1,8 @@
 package com.techacademy.service;
 
 import java.util.List;
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -21,9 +23,22 @@ public class UserService {
         return userRepository.findAll();
     }
     
+    /** Userを1件検索して返す */
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+    
     /** Userの登録を行なう */
     @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
+    }
+    
+    /** Userの削除を行なう */
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id : idck) {
+            userRepository.deleteById(id);
+        }
     }
 }
